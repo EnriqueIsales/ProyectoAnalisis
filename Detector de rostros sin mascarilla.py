49094e82f1e440bc998f68950a5f9318bd7a0c53
EnriqueIsales <84251533+EnriqueIsales@users.noreply.github.com>
2021-05-22 14:11:22 -0600

    Update Detector de rostros sin mascarilla.py

--- Detector de rostros sin mascarilla.py
+++ Detector de rostros sin mascarilla.py
@@ -23,6 +23,31 @@ count = 0
 
 
 
+while True:
+    ret, frame = cap.read()
+    gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    copia= frame.copy()
+
+    #detector de rostro 
+    caras= detector.detect_faces(copia)
+
+
+
+    for i in range(len (caras)):
+        x1,y1, ancho, alto = caras[i]['box']
+        x2,y2 = x1 + ancho, y1 + alto
+        cara_reg = frame [y1:y2, x1:x2]
+        cara_reg = cv2.resize(cara_reg, (150,200), interpolation = cv2.INTER_CUBIC)
+        cv2.imwrite(carpeta + "/rostro_{}.jpg".format(count), cara_reg)
+        count = count + 1
+    cv2.imshow("Entrenamiento", frame)
+
+    t= cv2.waitKey(1)
+    if t== 27 or count >=300:
+        break 
+
+cap.realease()
+cv2.destroyAllWindows()
 
 
 
